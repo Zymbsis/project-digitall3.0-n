1cc2ff84cb083b7ca1b1c00614f15aa104846d27
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-07-10 22:32:17 +0200

    added port

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 console.log('Have Fun ;-)');
