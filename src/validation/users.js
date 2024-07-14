1b9e62d70be041927a8c0c002c85ee83401ae0ec
spalnique <136997639+spalnique@users.noreply.github.com>
2024-07-14 10:37:26 +0000

    minor fixes in updateUserSchema

--- src/validation/users.js
+++ src/validation/users.js
@@ -12,10 +12,10 @@ export const loginUserSchema = Joi.object({
 
 export const updateUserSchema = Joi.object({
   email: Joi.string().email(),
-  name: Joi.string(),
+  name: Joi.string().min(3).max(50),
   gender: Joi.string().valid('man', 'woman'),
   dailyNorma: Joi.number().min(0).max(10000),
-  weight: Joi.number().min(0),
+  weight: Joi.number().min(0).max(700),
   activeHours: Joi.number().min(0).max(12),
   userId: Joi.string(),
   avatar: Joi.string(),
