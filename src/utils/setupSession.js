9ff690a920afc5f92455aad3f0f97cde35672e30
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-07-16 00:44:58 +0200

    parser

--- src/utils/setupSession.js
+++ src/utils/setupSession.js
@@ -4,13 +4,13 @@ export const setupSession = (res, session) => {
   res.cookie('refreshToken', session.refreshToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'Strict',
+    sameSite: 'None',
     expires: new Date(Date.now() + THIRTY_DAYS),
   });
   res.cookie('sessionId', session._id, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'Strict',
+    sameSite: 'None',
     expires: new Date(Date.now() + THIRTY_DAYS),
   });
 };
