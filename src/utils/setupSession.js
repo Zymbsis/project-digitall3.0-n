aecc084821079b1eda1e53d385584bdd864d2170
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-07-16 00:21:41 +0200

    .

--- src/utils/setupSession.js
+++ src/utils/setupSession.js
@@ -3,10 +3,12 @@ import { THIRTY_DAYS } from '../constants/index.js';
 export const setupSession = (res, session) => {
   res.cookie('refreshToken', session.refreshToken, {
     httpOnly: true,
+    secure: true,
     expires: new Date(Date.now() + THIRTY_DAYS),
   });
   res.cookie('sessionId', session._id, {
     httpOnly: true,
+    secure: true,
     expires: new Date(Date.now() + THIRTY_DAYS),
   });
 };
