24ba5e74da6b7c9baad55974254e72cef1023a9a
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-07-16 01:42:34 +0200

    refresh

--- src/utils/setupSession.js
+++ src/utils/setupSession.js
@@ -3,13 +3,13 @@ import { THIRTY_DAYS } from '../constants/index.js';
 export const setupSession = (res, session) => {
   res.cookie('refreshToken', session.refreshToken, {
     httpOnly: true,
-
+    secure: process.env.NODE_ENV === 'production',
     sameSite: 'None',
     expires: new Date(Date.now() + THIRTY_DAYS),
   });
   res.cookie('sessionId', session._id, {
     httpOnly: true,
-
+    secure: process.env.NODE_ENV === 'production',
     sameSite: 'None',
     expires: new Date(Date.now() + THIRTY_DAYS),
   });
