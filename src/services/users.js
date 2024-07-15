0a6b5171177101be5225732fce055486834ab6b2
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-07-15 17:49:12 +0200

    test

--- src/services/users.js
+++ src/services/users.js
@@ -53,20 +53,20 @@ export const logoutUser = async (sessionId) => {
 
 //REFRESH_USER//
 export const refreshUserSession = async ({ sessionId, refreshToken }) => {
-  // const session = await SessionsCollection.findOne({
-  //   _id: sessionId,
-  //   refreshToken,
-  // });
-  // if (!session) {
-  //   throw createHttpError(401, 'Session not found, refresh');
-  // }
-  // const isSessionTokenExpired =
-  //   new Date() > new Date(session.refreshTokenValidUntil);
-  // if (isSessionTokenExpired) {
-  //   throw createHttpError(401, 'Session token expired');
-  // }
+  const session = await SessionsCollection.findOne({
+    userId: sessionId,
+    refreshToken,
+  });
+  if (!session) {
+    throw createHttpError(401, 'Session not found, refresh');
+  }
+  const isSessionTokenExpired =
+    new Date() > new Date(session.refreshTokenValidUntil);
+  if (isSessionTokenExpired) {
+    throw createHttpError(401, 'Session token expired');
+  }
   await SessionsCollection.deleteOne({ _id: sessionId, refreshToken });
-  const newSession = await createSession(sessionId.userId);
+  const newSession = await createSession(session.userId);
 
   return newSession;
 };
