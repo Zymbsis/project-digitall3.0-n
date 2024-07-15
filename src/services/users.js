619e7e5694b503f926843bcf25ce4ebe29edb35d
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-07-15 17:33:49 +0200

    test

--- src/services/users.js
+++ src/services/users.js
@@ -58,7 +58,7 @@ export const refreshUserSession = async ({ sessionId, refreshToken }) => {
     refreshToken,
   });
   if (!session) {
-    throw createHttpError(401, 'Session not found');
+    throw createHttpError(401, 'Session not found, refresh');
   }
   const isSessionTokenExpired =
     new Date() > new Date(session.refreshTokenValidUntil);
