d29c8d457cf8e74203d2d523616fac090ba6a01f
spalnique <lex.sydoruk@gmail.com>
2024-07-18 22:40:45 +0200

    minor fixes

--- src/validation/users.js
+++ src/validation/users.js
@@ -2,12 +2,12 @@ import Joi from 'joi';
 
 export const registerUserSchema = Joi.object({
   email: Joi.string().email().required(),
-  password: Joi.string().required(),
+  password: Joi.string().min(8).max(64).required(),
 });
 
 export const loginUserSchema = Joi.object({
   email: Joi.string().email().required(),
-  password: Joi.string().required(),
+  password: Joi.string().min(8).max(64).required(),
 });
 
 export const updateUserSchema = Joi.object({
