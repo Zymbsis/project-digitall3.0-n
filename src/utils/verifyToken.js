23cc3f72f126ebae347984455c9d743a219323bc
spalnique <lex.sydoruk@gmail.com>
2024-07-23 02:56:22 +0200

    imports fixed

--- src/utils/verifyToken.js
+++ src/utils/verifyToken.js
@@ -1,6 +1,9 @@
 import createHttpError from 'http-errors';
 import jwt from 'jsonwebtoken';
 
+import { ENV_VARS } from '../constants/index.js';
+import { env } from './env.js';
+
 export const verifyToken = (token) => {
   try {
     return jwt.verify(token, env(ENV_VARS.JWT_SECRET));
