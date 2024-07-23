ca5197dbc11ec58be81cb63f1ded3378046f177f
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-07-23 12:14:57 +0200

    .

--- src/utils/verifyToken.js
+++ src/utils/verifyToken.js
@@ -1,13 +1,7 @@
 import createHttpError from 'http-errors';
-<<<<<<< HEAD
 import { ENV_VARS } from '../constants/index';
 import { env } from './env';
-=======
->>>>>>> 9394ce79bc5184fb30f37deeec2138b7cd75726b
 import jwt from 'jsonwebtoken';
-
-import { ENV_VARS } from '../constants/index.js';
-import { env } from './env.js';
 
 export const verifyToken = (token) => {
   try {
